63f1e99f394fea5c60b40ce088197c49ee1dc878
FernandoMelim <fernando.lief@hotmail.com>
2022-08-20 23:41:50 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,80 @@
+from gurobipy import gurobipy
+from decimal import *
+
+file = open('input.csv')
+lines = file.readlines()
+file.close()
+
+endlist = []
+
+# formatando
+for i in lines:
+    endlist.append(i.replace("\n", "").replace(',', '.'))
+
+# criando modelo
+model = gurobipy.Model('doceria')
+
+# declarando variaveis
+vars = []
+for i in endlist[0].split(';'):
+    vars.append(model.addVar(vtype=gurobipy.GRB.CONTINUOUS, name=i))
+
+# model.update()
+
+# definindo custos
+costs = []
+for i in endlist[1].split(';'):
+    costs.append(i)
+
+# montando função objetivo
+expression = ''
+for i in range(0, len(endlist[0].split(';'))):
+    expression += float(costs[i]) * vars[i]
+
+model.setObjective(expression, gurobipy.GRB.MINIMIZE)
+
+# montando restrições
+k = 1
+for i in range(2, len(endlist)):
+
+    consts = []
+    endlistSplited = endlist[i].split(';')
+
+    for j in range(0, len(endlistSplited) - 2):
+        consts.append(endlistSplited[j])
+
+    expression = ''
+    for j in range(0, len(consts)):
+        expression += consts[j] * vars[j]
+
+    if (endlistSplited[-2] == '='):
+        expression = expression == float(endlistSplited[-1])
+    elif (endlistSplited[-2] == '<='):
+        expression = expression <= float(endlistSplited[-1])
+    elif (endlistSplited[-2] == '>='):
+        expression = expression >= float(endlistSplited[-1])
+    elif (endlistSplited[-2] == '<'):
+        expression = expression < float(endlistSplited[-1])
+    elif (endlistSplited[-2] == '>'):
+        expression = expression > float(endlistSplited[-1])
+
+    model.addConstr(expression, "r" + str(k))
+    k += 1
+
+# otimizando
+model.optimize()
+
+results = model.getAttr('X')
+
+# escrevendo resultado em arquivo
+f = open("output.txt", "w")
+
+f.write("Custo total minimizado: R$ " +
+        str(model.ObjVal).replace('.', ',') + '\n')
+
+for i in range(0, len(vars)):
+    f.write(str(vars[i]).replace('<gurobi.Var ', '')
+            .replace(' (value ', ' - ').replace(')>', '')
+            .replace('.', ',') + '\n')
+
+f.close()
---
+++ teste.py
@@ -0,0 +1,23 @@
+from re import A
+import gurobipy as gp
+
+m = gp.Model("teste")
+
+x = m.addVar(vtype=gp.GRB.BINARY, name="x")
+y = m.addVar(vtype=gp.GRB.BINARY, name="y")
+z = m.addVar(vtype=gp.GRB.BINARY, name="z")
+m.update()
+
+a = x + y + 2*z
+m.setObjective(a, gp.GRB.MAXIMIZE)
+print("a ---------------------------------")
+print(a)
+
+b = x + 2*y + 3*z
+a = b <= 4
+m.addConstr(a, name="c0")
+m.addConstr(x + y >= 1, name="c1")
+
+m.optimize()
+
+
